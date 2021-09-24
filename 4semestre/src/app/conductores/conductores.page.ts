f67a1067830292f3d1852648251e91a26083465d
Zevagumbel <sebas.gomezs@duocuc.cl>
2021-09-24 20:14:10 -0300

    Commit conduc. y pasaj.

--- 4semestre/src/app/conductores/conductores.page.ts
+++ 4semestre/src/app/conductores/conductores.page.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./conductores.page.scss'],
 })
 export class ConductoresPage implements OnInit {
-
+  private cond = [
+    {
+      id:'1',
+      nombre:'Nicolas',
+      imgURL:'https://www.mobachampion.com/imgs/champion/highres/veigar.jpg'
+    },
+    {
+      id:'2',
+      nombre:'Diego',
+      imgURL:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWJbdiA0NUU3Nr0NEhYmF8z5rwfA-PnFXZZA&usqp=CAU'
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
--- 4semestre/src/app/pasajeros/pasajeros.page.ts
+++ 4semestre/src/app/pasajeros/pasajeros.page.ts
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pasajeros.page.scss'],
 })
 export class PasajerosPage implements OnInit {
+  private psj = [
+    {
+      id:'1',
+      nombre:'Diego Concha',
+      imgURL:'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Zac_1.jpg'
+    },
+    {
+      id:'2',
+      nombre:'Seba',
+      imgURL:'https://cdn.mos.cms.futurecdn.net/VcYnJP9V8ALcTA4qjow6PM.jpg'
+    }
+  ]
 
   constructor() { }
 
