5252c0b1b554c895145b0f1a96fe9738483290a4
Nicolas <nico.pina.navas@gmail.com>
2021-09-28 17:58:13 -0300

    Animations

--- 4semestre/src/app/contactos/contactos.page.ts
+++ 4semestre/src/app/contactos/contactos.page.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit} from '@angular/core';
+import { Animation, AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-contactos',
   templateUrl: './contactos.page.html',
   styleUrls: ['./contactos.page.scss'],
 })
-export class ContactosPage implements OnInit {
+export class ContactosPage {
 
-  constructor() { }
+  constructor(private animationCtrl: AnimationController) { 
+  }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+
+  const square = this.animationCtrl
+  .create()
+  .addElement(document.querySelector('.square'))
+  .duration(1600)
+  .iterations(0.86)
+  .fromTo('transform', 'translateX(0px)', 'translateX(97px)')
+  .fromTo('opacity', '0.1', '2');
+
+  const squareb = this.animationCtrl
+  .create()
+  .addElement(document.querySelector('.squareb'))
+  .duration(1600)
+  .iterations(0.86)
+  .fromTo('transform', 'translateX(0px)', 'translateX(97px)')
+  .fromTo('opacity', '0.1', '2');
+    
+    square.play(
+    )
+    squareb.play(
+      )
   }
 
 }
