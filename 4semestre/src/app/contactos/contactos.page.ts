fb244150b6112b5a2565a3659454f1c28cc6ea50
Nicolas <nico.pina.navas@gmail.com>
2021-09-28 21:28:04 -0300

    actualizacion

--- 4semestre/src/app/contactos/contactos.page.ts
+++ 4semestre/src/app/contactos/contactos.page.ts
@@ -18,7 +18,7 @@ export class ContactosPage {
   .addElement(document.querySelector('.Firstsquare'))
   .duration(1600)
   .iterations(0.86)
-  .fromTo('transform', 'translateX(0px)', 'translateX(107px)')
+  .fromTo('transform', 'translateX(0px)', 'translateX(110px)')
   .fromTo('opacity', '0.1', '3');
 
 
@@ -27,7 +27,7 @@ export class ContactosPage {
   .addElement(document.querySelector('.square2do'))
   .duration(1600)
   .iterations(0.86)
-  .fromTo('transform', 'translateX(0px)', 'translateX(107px)')
+  .fromTo('transform', 'translateX(0px)', 'translateX(110px)')
   .fromTo('opacity', '0.1', '3');
     
   Firstsquare.play(
